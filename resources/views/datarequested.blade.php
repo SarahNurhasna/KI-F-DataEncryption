18fd5d8c29aad21bdef97e3b7367f70e4ba0388a
Sarah Nurhasna <sarah2501.snk@gmail.com>
2023-12-01 10:50:32 +0700

    edit fe

--- resources/views/datarequested.blade.php
+++ resources/views/datarequested.blade.php
@@ -1,11 +1,18 @@
 @extends('main')
 
 @section('container')
-<h2>Data Requested</h2>
+    <div class="data-requested">
+        <h2>Data Requested</h2>
 
-<p>Data has been requested </br> Check your <a href="inbox">inbox</a> regularly and enter the symmetric key to get the data</p>
-    <div class="form-group">
-        <input type="age" id="age" required />
+        <p>Data has been requested </br> Check your email</a> regularly and enter the symmetric key to get the data</p>
+        <div class="form-group">
+            <input type="age" id="age" required />
+        </div>
+        <button id="send-button" class="btn btn-rounded" type="submit">Submit</button>  
     </div>
-    <button id="send-button" class="btn btn-rounded" type="submit">Submit</button>
+    
+    <div class="data-user">
+        <h2>Data User</h2>
+    </div>
+    
 @endsection
